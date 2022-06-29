d2eabb1c6e25d07a93a57eae8974b8980655babe
gPass0s <guilhermepassos@outlook.com>
2022-06-29 08:31:55 -0300

    Add to project terraform modules for kinesis data stream resources

---
+++ infra-as-code/modules/kinesis-data-stream/variables.tf
@@ -0,0 +1,13 @@
+variable "ENV" {}
+
+variable "PROJECT_NAME" {}
+
+variable "NUMBER_OF_SHARDS" {
+  default = 1
+}
+
+variable "RETENTION_PERIOD" {
+  default = 24
+}
+
+variable "RESOURCE_SUFFIX" {}
